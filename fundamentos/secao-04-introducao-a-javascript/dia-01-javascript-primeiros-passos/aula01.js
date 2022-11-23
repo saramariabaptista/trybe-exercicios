477b7efb41874972c2eff99bf910afbff72fb8e8
Sara Maria Baptista <smr.baptiste@gmail.com>
2022-11-23 15:32:44 -0300

    adiciona exercicio de aula 01

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/aula01.js
@@ -0,0 +1,22 @@
+/* Exercícios de aula 
+
+Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
+Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? */
+
+const myName='Sara';
+const birthCity='Rio de Janeiro';
+let birthYear=1994;
+console.log(myName, birthCity, birthYear);
+birthYear=2030;
+console.log(birthYear);
+birthCity = 'sao paulo';
+
+// O erro acontece porque birthCity é uma constante e seu valor não pode ser alterado. // 
+
+
+
+
