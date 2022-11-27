375050e981915df4ec6f720adb683744f82c86ea
Sara Maria Baptista <smr.baptiste@gmail.com>
2022-11-27 13:34:42 -0300

    exercicio 02 concluido

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
@@ -7,6 +7,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index=0; index<numbers.length; index +=1){
     console.log(numbers[index]);
-}  */
+}  
 
+Some todos os valores contidos no array e imprima o resultado;
+
+for (let index=0; index<numbers.length; index+=1){
+    console.log(numbers[index]+numbers[index]);
+}*/
 
