377eb09f7a922c73b2e40d6442e619c8b113b2d5
Sara Maria Baptista <smr.baptiste@gmail.com>
2022-11-27 15:26:26 -0300

    adiciona exercicios do 06 ao 09

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
@@ -1,8 +1,8 @@
-/*  Iremos utilizar esse array para realizar os próximos exercícios. 
+/*  Iremos utilizar esse array para realizar os próximos exercícios.
 
  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-Exercicio 01 - Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+/*Exercicio 01 - Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index=0; index<numbers.length; index +=1){
@@ -33,9 +33,7 @@ if (average >20){
 }
 console.log(mensagem); 
 
-Exercicio 05 - Utilizando for, descubra qual o maior valor contido no array e imprima-o;*/
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+Exercicio 05 - Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
 let maiorNumero=0;
 for (let index = 1; index < numbers.length; index += 1) {
@@ -45,3 +43,45 @@ for (let index = 1; index < numbers.length; index += 1) {
   }
   
   console.log(maiorNumero);
+
+Exercicio 06 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”; 
+
+let resultado=0;
+
+for (let index=0; index<numbers.length; index+=1){
+    if (numbers[index] % 2 !== 0) {
+        resultado += 1;
+      }
+    }
+if (resultado === 0) {
+      console.log('nenhum valor ímpar encontrado');
+    } else {
+      console.log(resultado);
+    }
+
+Exercício 07 -  Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+let menorNumero= numbers[0];
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] < menorNumero) {
+      menorNumero = numbers[index];
+    }
+  }
+  
+  console.log(menorNumero); 
+
+Exercício 08 - Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado.*/
+
+  let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+/* console.log(numbers);  */
+
+/* Exercício 09 - Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2. */
+
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log( 'O resultado da divisão é ' + numbers[index] / 2);
+  };
