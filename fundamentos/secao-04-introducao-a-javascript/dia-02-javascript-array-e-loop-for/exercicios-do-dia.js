bad65276275884f193630ca7990eae5e80145da0
Sara Maria Baptista <smr.baptiste@gmail.com>
2022-11-27 14:06:58 -0300

    exercicio 05 concluido

--- fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicios-do-dia.js
@@ -1,29 +1,29 @@
-/*  Iremos utilizar esse array para realizar os próximos exercícios. */
+/*  Iremos utilizar esse array para realizar os próximos exercícios. 
 
  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-/* Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+Exercicio 01 - Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index=0; index<numbers.length; index +=1){
     console.log(numbers[index]);
 }  
 
-Some todos os valores contidos no array e imprima o resultado; 
+Exercicio 02 - Some todos os valores contidos no array e imprima o resultado; 
 let result=0;
 for (let index=0; index<numbers.length; index+=1){
     result += numbers[index];
 } console.log(result)
 
-Calcule e imprima a média aritmética dos valores contidos no array;
-- A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos. */
+Exercicio 03 - Calcule e imprima a média aritmética dos valores contidos no array;
+- A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos. 
 let sum=0;
 for (let index=0; index<numbers.length; index +=1){
     sum += numbers[index];
 }
 let average= sum/numbers.length;
 
-/* Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”; */
+Exercicio 04 - Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”; 
 
 let mensagem =0;
 if (average >20){
@@ -31,4 +31,17 @@ if (average >20){
 } if (average <= 20) {
     mensagem= 'valor menor ou igual a 20'
 }
-console.log(mensagem);
+console.log(mensagem); 
+
+Exercicio 05 - Utilizando for, descubra qual o maior valor contido no array e imprima-o;*/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero=0;
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > maiorNumero) {
+      maiorNumero = numbers[index];
+    }
+  }
+  
+  console.log(maiorNumero);
